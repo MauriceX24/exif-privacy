736e158a77b47d30cd67571099edf27278a5ebab
Maurice <35397688+MauriceX24@users.noreply.github.com>
2023-03-05 16:16:12 +0100

    Add files via upload

---
+++ EXIF-Privacy/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(123, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Read"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(141, 38)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(147, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Delete Data and &Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.AllowDrop = True
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(304, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(336, 420)
+        Me.ListBox1.TabIndex = 2
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.EXIF_Privacy.My.Resources.Resources.Unbenannt
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 83)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(276, 249)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 428)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Status:"
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(653, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "EXIF-Privacy"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ EXIF-Privacy/Form1.vb
@@ -0,0 +1,101 @@
+﻿Imports System.Drawing.Imaging
+Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button2.Enabled = False
+        Label1.Text = $"Status: nothing."
+    End Sub
+    Dim choosenPic As String
+    Dim dragNDropPic As String
+    Dim choosenFormat As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+        OpenFileDialog1.Filter = "PNG|*.png|jpeg|*.jpeg|JPG|*.jpg"
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            Dim filePic As String = OpenFileDialog1.FileName
+            choosenPic = OpenFileDialog1.FileName
+            choosenFormat = OpenFileDialog1.FileName.Split(".")(1)
+            Dim image As Image = Image.FromFile(filePic)
+            PictureBox1.Image = image
+            Dim propertyItems As PropertyItem() = image.PropertyItems
+
+            For Each item As PropertyItem In propertyItems
+                'Read Items
+                ListBox1.Items.Add(item.Id & ": " & System.Text.Encoding.ASCII.GetString(item.Value))
+            Next
+            Button2.Enabled = True
+            Label1.Text = $"Status: Ready to hide your datas"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim image As Image = Image.FromFile(choosenPic)
+        Dim propertyItems As PropertyItem() = image.PropertyItems
+        Dim counter As Integer = 0
+        For Each item As PropertyItem In propertyItems
+            'Change Items
+            If item.Id = 33432 Then 'EXIF-Copyright'
+                item.Value = System.Text.Encoding.UTF8.GetBytes("Copyright © Lordmaurice.xyz")
+                image.SetPropertyItem(item)
+            Else
+                item.Value = System.Text.Encoding.ASCII.GetBytes(" ")
+                image.SetPropertyItem(item)
+            End If
+            counter += 1
+        Next
+
+        If choosenPic.Contains(".jpeg") Or choosenPic.Contains(".jpg") Or choosenPic.Contains(".png") Then
+            Dim s As String
+            s = choosenPic.Split(".")(0).Replace("png", "")
+            s = choosenPic.Split(".")(0).Replace("jpg", "")
+            s = choosenPic.Split(".")(0).Replace("jpeg", "")
+            image.Save($"{s}({TimeOfDay.Hour & TimeOfDay.Minute & TimeOfDay.Second}) -PRIVACY.{choosenFormat}")
+            checkAfterChange($"{s}({TimeOfDay.Hour & TimeOfDay.Minute & TimeOfDay.Second}) -PRIVACY.{choosenFormat}")
+        End If
+        Button2.Enabled = False
+        Label1.Text = $"Status: Datas cleared - {counter} items cleared"
+    End Sub
+    Private Function checkAfterChange(ByVal pic As String)
+        ListBox1.Items.Clear()
+        Dim image As Image = Image.FromFile(pic)
+        Dim propertyItems As PropertyItem() = image.PropertyItems
+
+        For Each item As PropertyItem In propertyItems
+            'Hier können Sie die Daten aus dem Objekt extrahieren'
+            ListBox1.Items.Add(item.Id & ": " & System.Text.Encoding.UTF8.GetString(item.Value))
+        Next
+    End Function
+    Private Function dragAndDropRead()
+        Try
+            ListBox1.Items.Clear()
+            Dim filePic As String = dragNDropPic
+            choosenPic = dragNDropPic
+            choosenFormat = dragNDropPic.Split(".")(1)
+            Dim image As Image = Image.FromFile(filePic)
+            PictureBox1.Image = image
+            Dim propertyItems As PropertyItem() = image.PropertyItems
+
+            For Each item As PropertyItem In propertyItems
+                'Hier können Sie die Daten aus dem Objekt extrahieren'
+                ListBox1.Items.Add(item.Id & ": " & System.Text.Encoding.ASCII.GetString(item.Value))
+            Next
+            Button2.Enabled = True
+            Label1.Text = $"Status: Ready to hide your datas"
+        Catch ex As Exception
+            Label1.Text = "Status: Could not read this"
+        End Try
+    End Function
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+        Try
+            Dim filePaths As String() = e.Data.GetData(DataFormats.FileDrop)
+            Dim filePath As String = filePaths(0)
+            Dim pic As Image = Image.FromFile(filePath)
+            PictureBox1.Image = pic
+            dragNDropPic = filePath
+            dragAndDropRead()
+        Catch ex As Exception
+            Label1.Text = "Status: Please choose another file"
+        End Try
+    End Sub
+End Class
